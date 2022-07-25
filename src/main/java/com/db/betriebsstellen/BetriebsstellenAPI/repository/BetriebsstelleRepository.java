084b0fb70fa3919ec4d80e30a69877edf761cad0
Laura <laura.mey@rwth-aachen.de>
2022-07-25 20:47:48 +0200

    create BetriebsstelleRepository

--- src/main/java/com/db/betriebsstellen/BetriebsstellenAPI/repository/BetriebsstelleRepository.java
+++ src/main/java/com/db/betriebsstellen/BetriebsstellenAPI/repository/BetriebsstelleRepository.java
@@ -1,4 +1,12 @@
 package com.db.betriebsstellen.BetriebsstellenAPI.repository;
 
-public interface BetriebsstelleRepository {
+import com.db.betriebsstellen.BetriebsstellenAPI.entity.Betriebsstelle;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BetriebsstelleRepository extends CrudRepository<Betriebsstelle, Integer> {
+    @Query("select * from Betriebsstelle b where b.abbrev=:abbrev")
+    String findBetriebsstelleByAbbrev(String abbrev); //TODO check for duplicates
 }
