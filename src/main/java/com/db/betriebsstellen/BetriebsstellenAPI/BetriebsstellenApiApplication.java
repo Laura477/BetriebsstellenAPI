d68a4a4f4f527b82aa36f35b6191fb312ca5740a
Laura <laura.mey@rwth-aachen.de>
2022-07-25 18:12:24 +0200

    initial commit (files created by Spring Initalizr)

---
+++ src/main/java/com/db/betriebsstellen/BetriebsstellenAPI/BetriebsstellenApiApplication.java
@@ -0,0 +1,13 @@
+package com.db.betriebsstellen.BetriebsstellenAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetriebsstellenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetriebsstellenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/betriebsstellen/BetriebsstellenAPI/BetriebsstellenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.betriebsstellen.BetriebsstellenAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetriebsstellenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
